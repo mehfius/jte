2a231ece90e12bb6b5c11e8953e24a5e4c440353
Matheus <matheus.ferraz@gmail.com>
2025-03-05 21:49:01 -0300

    Add debug logging feature for JTE with dev attribute

--- src/jte.js
+++ src/jte.js
@@ -1,4 +1,20 @@
 function jte(json) {
+    // Get the current script element
+    const scripts = document.getElementsByTagName('script');
+    const currentScript = scripts[scripts.length - 1];
+    
+    // Debug logging if dev="true"
+    if (currentScript.getAttribute('dev') === 'true') {
+        console.log(
+            '%c %c JTE Debug \n' + 
+            'Element: ' + JSON.stringify(json, null, 2) + 
+            ' %c ',
+            'background: red; padding: 5px 0;',
+            'background: red; color: white; padding: 5px 0;',
+            'background: red; padding: 5px 0;'
+        );
+    }
+
     const field = document.createElement(json.tag);
     const specialHandlers = ['innerhtml', 'tag', 'textnode', 'value'];
     const eventHandlers = ['onclick', 'onchange', 'onkeyup', 'onkeypress', 'oninput', 'onfocus', 'onblur'];
